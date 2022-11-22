2a719ea8da32a25b875accec9cf3d571614a9513
root <root@513dec29e801>
2022-11-22 02:26:24 +0000

    popcorn maker

---
+++ src/_03_cat/catRunner.java
@@ -0,0 +1,10 @@
+package _03_cat;
+
+public class catRunner {
+public static void main(String[] args) {
+Cat cat = new Cat("Ellie");
+cat.meow();
+cat.printName();
+cat.kill();
+}
+}
---
+++ src/_05_popcorn/PopcornMaker.java
@@ -0,0 +1,8 @@
+package _05_popcorn;
+
+public class PopcornMaker {
+public static void main(String[] args) {
+	Popcorn pc = new Popcorn("Butter");
+	pc.putInMicrowave();
+}
+}
